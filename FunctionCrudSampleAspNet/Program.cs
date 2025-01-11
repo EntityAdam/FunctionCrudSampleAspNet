d7b61d20340232fc8a5e17ea8664c2296d3ae34a
Adam Vincent <adam.vincent@ais.com>
2025-01-10 21:41:10 -0500

    configure jsonserializer

--- FunctionCrudSampleAspNet/Program.cs
+++ FunctionCrudSampleAspNet/Program.cs
@@ -1,6 +1,24 @@
+using Azure.Core.Serialization;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 var builder = FunctionsApplication.CreateBuilder(args);
+
 builder.ConfigureFunctionsWebApplication();
-builder.Build().Run();
+
+// Configuring the functions worker with a the Azure Core JsonSerializer
+// that will work with CosmosDbInput / CosmosDbOutput bindings.
+builder.Services.Configure<WorkerOptions>(options =>
+{
+    options.Serializer = new JsonObjectSerializer(new JsonSerializerOptions
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+    });
+});
+
+await builder.Build().RunAsync();
